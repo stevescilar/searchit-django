30b194e1dd80c90bdfbe07a0d691f5276b7900d7
stevescilar <stevemuambi@gmail.com>
2022-03-23 09:19:44 +0300

    update

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import HttpResponse
+
+
+def projects(request):
+    return HttpResponse('here is where products dwell')
+
+def  project(request,pk):
+    return HttpResponse('Project Detail page')
