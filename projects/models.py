e363bcd46df2978730ecdeb6731ce5e6cd6e4aa5
stevescilar <stevemuambi@gmail.com>
2022-03-28 11:56:09 +0300

    setting up

--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 200)
+    description = models.TextField(null=True,blank=True)
+    demo_link = models.Charfield(max_length=2000,null=True,blank=True)
+    source_link = models.CharField(max_length=2000,null=True,blank=True)
